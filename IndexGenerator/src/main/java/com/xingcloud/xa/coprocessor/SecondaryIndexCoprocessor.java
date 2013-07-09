71f856e71f78cf8375dd740f3c02ed73b107ac8d
Snake <snake.fly@hotmail.com>
2013-07-09 18:13:44 +0800

    change to only one attribute table

--- IndexGenerator/src/main/java/com/xingcloud/xa/coprocessor/SecondaryIndexCoprocessor.java
+++ IndexGenerator/src/main/java/com/xingcloud/xa/coprocessor/SecondaryIndexCoprocessor.java
@@ -1,10 +1,15 @@
 package com.xingcloud.xa.coprocessor;
 
 import com.google.protobuf.ServiceException;
+import com.xingcloud.userprops_meta_util.UpdateFunc;
+import com.xingcloud.userprops_meta_util.UserProp;
+import com.xingcloud.userprops_meta_util.UserProps_DEU_Util;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.CoprocessorEnvironment;
 import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.KeyValueUtil;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.coprocessor.BaseRegionObserver;
 import org.apache.hadoop.hbase.coprocessor.ObserverContext;
@@ -20,6 +25,7 @@ import org.codehaus.jackson.map.ObjectMapper;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * User: Jian Fang
@@ -30,9 +36,10 @@ public class SecondaryIndexCoprocessor extends BaseRegionObserver {
     private HTablePool pool;
     private ObjectMapper mapper;
 
-    private Map<String, Map<String, Integer>> properties = new HashMap<String, Map<String, Integer>>();
-  
+    private static Map<String, Map<Integer, UpdateFunc>> metaInfo = new ConcurrentHashMap<String, Map<Integer, UpdateFunc>>();
     private static Log LOG = LogFactory.getLog(SecondaryIndexCoprocessor.class);
+
+    private static final byte[] CF_NAME = Bytes.toBytes("VAL");
 
     @Override
     public void start(CoprocessorEnvironment e){
@@ -52,75 +59,83 @@ public class SecondaryIndexCoprocessor extends BaseRegionObserver {
         if(!tableName.startsWith("property_") || tableName.endsWith("_index")){
             return;
         }
-//        int index = tableName.lastIndexOf("_");
-//        String projectID = tableName.substring(tableName.indexOf("_")+1, index);//sof-dsk
-//        int propertyID = Integer.parseInt(tableName.substring(index + 1));
 
         String[] fields = tableName.split("_");
         String projectID = fields[1];
-        int propertyID = Integer.parseInt(fields[2]);
-        String updateFunc = fields[3];
 
-
-        long s1 = System.nanoTime();
         HTableInterface dataTable = observerContext.getEnvironment().getTable(table);
-        List<KeyValue> values = put.get(Bytes.toBytes("value"), Bytes.toBytes("value"));
-        byte[] newValue = {};
-        if(values.size() > 0) newValue = values.get(0).getValue();
-        byte[] oldValue = new byte[0];
+
+        List<byte[]> qualifierList = new ArrayList<byte[]>();
+        Map<Integer, byte[]> cache = new HashMap<Integer, byte[]>();
+
+        for (Cell cell : put.getFamilyMap().get("val")) {
+            KeyValue kv = KeyValueUtil.ensureKeyValue(cell);
+            byte[] qualifier = kv.getQualifier();
+            qualifierList.add(qualifier);
+            cache.put(Bytes.toInt(qualifier), kv.getValue());
+        }
+
+        LOG.info("Num of puts for one row: " + cache.size());
+
+        //Get old values which related to the qualifier
+        KeyValue[] oldValues = null;
         try {
-            oldValue = getValue(observerContext.getEnvironment().getRegion().getRegionName(), put.getRow());
+            long s1 = System.nanoTime();
+            oldValues = getValue(observerContext.getEnvironment().getRegion().getRegionName(), put.getRow(), qualifierList);
+            LOG.info("Old value size: " + oldValues.length + " Taken: " + (System.nanoTime()-s1)/1.0e9 + " sec");
         } catch (ServiceException e) {
             e.printStackTrace();
             LOG.error(e.getMessage());
             return;
         }
 
-        long s2 = System.nanoTime();
-        if(oldValue == null){
-            submitIndexJob(projectID, false, put.getRow(), propertyID, null, newValue);
-        } else if(!Bytes.equals(oldValue, newValue)){
-            if (updateFunc.equals("c")) {
-                //Cover update manner
-                submitIndexJob(projectID, true, put.getRow(), propertyID, oldValue, newValue);
+        Map<Integer, UpdateFunc> metaMap = getMetaInfo(projectID);
+
+        //Put attributes which already exist in table
+        for (KeyValue kv : oldValues) {
+            int qualifier = Bytes.toInt(kv.getQualifier());
+            byte[] oldValue = kv.getValue();
+            byte[] newValue = cache.get(qualifier);
+
+            UpdateFunc uf = metaMap.get(qualifier);
+            if (uf == null) {
+                //Reload meta info
+                metaMap = getMetaInfo(projectID);
+                uf = metaMap.get(qualifier);
+                if (uf == null) {
+                    LOG.error("Attribute: [" + qualifier + "] doesn't exist in meta table!");
+                    return;
+                }
             }
+
+            if (!Bytes.equals(oldValue, newValue)) {
+                 //Update attribute value and ignore once
+                 if (uf == UpdateFunc.cover) {
+                    submitIndexJob(projectID, true, put.getRow(), qualifier, oldValue, newValue);
+                 }
+            } else if (uf == UpdateFunc.inc) {
+                 //Increment attribute value
+                 byte[] result = Bytes.toBytes(Bytes.toLong(oldValue) + Bytes.toLong(newValue));
+                 submitIndexJob(projectID, true, put.getRow(), qualifier, oldValue, result);
+            }
+            cache.remove(qualifier);
         }
-        long s3 = System.nanoTime();
+
+        //Put remain attributes which don't exist in table before
+        for (Map.Entry<Integer, byte[]> entry : cache.entrySet()) {
+            int qualifier = entry.getKey();
+            byte[] val = entry.getValue();
+            submitIndexJob(projectID, false, put.getRow(), qualifier, null, val);
+        }
+
         dataTable.close();
     }
 
-    @Override
-    public Result postIncrement(final ObserverContext<RegionCoprocessorEnvironment> observerContext,
-                                final Increment increment, final Result result) throws IOException {
-        byte[] table  = observerContext.getEnvironment().getRegion().getRegionInfo().getTableName();
-        String tableName = Bytes.toString(table);
-        if(!tableName.startsWith("property_") || tableName.endsWith("_index")){
-            return result;
-        }
-
-        String[] fields = tableName.split("_");
-        String projectID = fields[1];
-        int propertyID = Integer.parseInt(fields[2]);
-
-        Map<byte[], NavigableMap<byte [], Long>> resultMap = increment.getFamilyMapOfLongs();
-        Map<byte[], Long> specificCol = resultMap.get(Bytes.toBytes("value"));
-
-        for (KeyValue kv : result.raw()) {
-            byte[] q = kv.getQualifier();
-            long incrementVal = specificCol.get(q);
-            long resultVal = Bytes.toLong(kv.getValue());
-            if (incrementVal == resultVal) {
-                submitIndexJob(projectID, false, kv.getRow(), propertyID, null, Bytes.toBytes(resultVal));
-            } else {
-                submitIndexJob(projectID, true, kv.getRow(), propertyID, Bytes.toBytes(resultVal-incrementVal), Bytes.toBytes(resultVal));
-            }
-        }
-        return result;
-    }
-
-
-    private byte[] getValue(byte[] regionName, byte[] uid) throws IOException, ServiceException {
+    private KeyValue[] getValue(byte[] regionName, byte[] uid, List<byte[]> qualifierList) throws IOException, ServiceException {
         Get get = new Get(uid);
+        for (byte[] qualifier : qualifierList) {
+            get.addColumn(CF_NAME, qualifier);
+        }
         ClientProtos.GetRequest request = RequestConverter.buildGetRequest(regionName, get);
         ClientProtos.GetResponse response = HRegionServerRegister.getLast().get(null, request);
 
@@ -130,7 +145,7 @@ public class SecondaryIndexCoprocessor extends BaseRegionObserver {
         if(result.isEmpty()){
             return null;
         } else {
-            return result.getValue(Bytes.toBytes("value"), Bytes.toBytes("value"));
+            return result.raw();
         }
     }
 
@@ -150,6 +165,22 @@ public class SecondaryIndexCoprocessor extends BaseRegionObserver {
         LOG.info(mapper.writeValueAsString(jobMap));
     }
 
+    private Map<Integer, UpdateFunc> getMetaInfo(String projectID) throws IOException {
+        Map<Integer, UpdateFunc> metaMap = metaInfo.get(projectID);
+        if (metaMap == null) {
+            long st = System.nanoTime();
+            List<UserProp> props = UserProps_DEU_Util.getInstance().getUserProps(projectID);
+            LOG.info("Scan property table finished. Property number: " +  props.size() + "Taken: " + (System.nanoTime()-st)/1.0e9 + " sec");
+            metaMap = new HashMap<Integer, UpdateFunc>();
+            for (UserProp up : props) {
+                int id = up.getId();
+                UpdateFunc uf = up.getPropFunc();
+                metaMap.put(id, uf);
+            }
+            metaInfo.put(projectID, metaMap);
+        }
+        return metaMap;
+    }
 
 
 }

fcfafc9bdff29c0bc91a3fad066e26a312e7f4ab
Snake <snake.fly@hotmail.com>
2013-08-22 14:24:05 +0800

    fix test case bug

--- IndexRebuild/src/test/java/com/xingcloud/xa/indextools/TestIndexREbuilder.java
+++ IndexRebuild/src/test/java/com/xingcloud/xa/indextools/TestIndexREbuilder.java
@@ -184,53 +184,53 @@ public class TestIndexREbuilder {
     }
   }
 
-  @AfterClass
-  public static void tearDownAfterClass() throws Exception {
-    //Cleanup meta table
-    HTable metaTable = null;
-    try {
-       metaTable = new HTable(conf, metaTableName);
-      List<Delete> deletes = new ArrayList<Delete>();
-      byte[] rk = Bytes.toBytes(pID + "_register_time");
-      Delete del = new Delete(rk);
-      deletes.add(del);
-
-      rk = Bytes.toBytes(pID + "_nation");
-      del = new Delete((rk));
-      deletes.add(del);
-
-      rk = Bytes.toBytes(pID + "_language");
-      del = new Delete(rk);
-      deletes.add(del);
-
-      metaTable.delete(deletes);
-
-      HBaseAdmin admin = new HBaseAdmin(conf);
-      admin.majorCompact(metaTableName);
-
-      //Drop test tables
-      admin.disableTable(indexTableName);
-      admin.deleteTable(indexTableName);
-
-      admin.disableTable(propTableName);
-      admin.deleteTable(propTableName);
-      LOG.info("Cleanup finish.");
-
-    } catch (IOException e) {
-      e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-    } catch (InterruptedException e) {
-      e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-    } finally {
-      if (metaTable != null) {
-        try {
-          metaTable.close();
-        } catch (IOException e) {
-          e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-      }
-    }
-
-  }
+//  @AfterClass
+//  public static void tearDownAfterClass() throws Exception {
+//    //Cleanup meta table
+//    HTable metaTable = null;
+//    try {
+//       metaTable = new HTable(conf, metaTableName);
+//      List<Delete> deletes = new ArrayList<Delete>();
+//      byte[] rk = Bytes.toBytes(pID + "_register_time");
+//      Delete del = new Delete(rk);
+//      deletes.add(del);
+//
+//      rk = Bytes.toBytes(pID + "_nation");
+//      del = new Delete((rk));
+//      deletes.add(del);
+//
+//      rk = Bytes.toBytes(pID + "_language");
+//      del = new Delete(rk);
+//      deletes.add(del);
+//
+//      metaTable.delete(deletes);
+//
+//      HBaseAdmin admin = new HBaseAdmin(conf);
+//      admin.majorCompact(metaTableName);
+//
+//      //Drop test tables
+//      admin.disableTable(indexTableName);
+//      admin.deleteTable(indexTableName);
+//
+//      admin.disableTable(propTableName);
+//      admin.deleteTable(propTableName);
+//      LOG.info("Cleanup finish.");
+//
+//    } catch (IOException e) {
+//      e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//    } catch (InterruptedException e) {
+//      e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//    } finally {
+//      if (metaTable != null) {
+//        try {
+//          metaTable.close();
+//        } catch (IOException e) {
+//          e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//        }
+//      }
+//    }
+//
+//  }
 
   @Test
   public void testReBuild() {
